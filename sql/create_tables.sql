0571034955bb3efc6c354e187d092f3cabd8766d
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2016-04-03 22:54:12 +0300

    tietokantataulut

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,52 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Trainer(
+	Id SERIAL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Trainername VARCHAR(50) NOT NULL,
+	Password VARCHAR(50) NOT NULL,
+	Weight INTEGER,
+	Height INTEGER,
+	Gender VARCHAR(10)
+);
+
+CREATE TABLE Goal(
+	Id SERIAL PRIMARY KEY,
+	TrainerId INTEGER REFERENCES Trainer(Id),
+	StartDate TIMESTAMP NOT NULL,
+	EndDate TIMESTAMP NOT NULL,
+	Exercise_name VARCHAR(100) NOT NULL,
+	Current_quantity INTEGER NOT NULL
+);
+
+CREATE TABLE Friendship(
+	Trainer1_Id INTEGER REFERENCES Trainer(Id), 
+	Trainer2_Id INTEGER REFERENCES Trainer(Id), 
+	Status INTEGER NOT NULL,
+	Action_trainerId INTEGER NOT NULL
+);
+
+CREATE TABLE Workout(
+	Id SERIAL PRIMARY KEY,
+	TrainerId INTEGER REFERENCES Trainer(Id),
+	WorkoutTime TIMESTAMP NOT NULL
+);
+
+CREATE TABLE CardioExercise(
+	Id SERIAL PRIMARY KEY,
+	WorkoutId INTEGER REFERENCES Workout(Id),
+	Name VARCHAR(50),
+	Duration INTEGER,
+	Distance INTEGER
+);
+
+CREATE TABLE WeightExercise(
+	Id SERIAL PRIMARY KEY,
+	WorkoutId INTEGER REFERENCES Workout(Id),
+	Name VARCHAR(50) NOT NULL,
+	Weight INTEGER NOT NULL
+);
+
+CREATE TABLE Set(
+	ExerciseId INTEGER REFERENCES WeightExercise(Id),
+	Reps INTEGER NOT NULL
+);
