266be3edd351652d14e4397cc94a868dfd451d8e
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2016-04-04 22:14:34 +0300

    jtn

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,5 +1,9 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
--- Käyttäjät
+-- Käyttäjät (Trainer)
 INSERT INTO Trainer (Name, Trainername, Password, Height_in_cm, Weight_in_kg, Gender) VALUES ('Vierailija', 'Vierailija', 'salasana', 200, 100, 'Male');
+INSERT INTO Trainer (Name, Trainername, Password, Height_in_cm, Weight_in_kg, Gender) VALUES ('Evan Miller', 'eamiller', 'salasana', 188, 86, 'Male');
+INSERT INTO Trainer (Name, Trainername, Password, Height_in_cm, Weight_in_kg, Gender) VALUES ('Axel Wallin', 'brodie', 'salasana', 100, 50, 'Male');
 
---
+-- 
+INSERT INTO Friendship (Trainer1_Id, Trainer2_Id, Status, Action_trainerId) VALUES (1, 2, 1, 1);
+INSERT INTO Friendship (Trainer1_Id, Trainer2_Id, Status, Action_trainerId) VALUES (1, 3, 0, 1);
